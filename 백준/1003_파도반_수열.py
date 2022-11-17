2be43b1761a68d1d0ead4130e1d0a42b3f6a28f5
LeeYoungJun <46258521+YJJonatanLee@users.noreply.github.com>
2022-11-17 12:24:19 +0900

    Update 1003_파도반_수열.py

--- 백준/1003_파도반_수열.py
+++ 백준/1003_파도반_수열.py
@@ -1,10 +1,13 @@
-n = 12
+T = int(input())
 
-memo = [0] * (n + 1)
-
-memo[1], memo[2], memo[3] = 1, 1, 1
-
-for i in range(4, n + 1):
-    memo[i] = memo[i - 2] + memo[i - 3]
+for _ in range(T):
+    n = int(input())
     
-print(memo[n])
+    memo = [0] * 100
+
+    memo[0], memo[1], memo[2] = 1, 1, 1
+
+    for i in range(3, n):
+        memo[i] = memo[i - 2] + memo[i - 3]
+        
+    print(memo[n - 1])
